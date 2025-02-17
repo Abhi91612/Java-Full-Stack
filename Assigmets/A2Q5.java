2a56d73928ca51c62d15be1e67fa29010a30f0f0
Abhishek kumar <abhishek08310@gmail.com>
2025-02-17 16:38:12 +0530

    Update A2Q5.java

--- Assigmets/A2Q5.java
+++ Assigmets/A2Q5.java
@@ -5,27 +5,26 @@ public class A2Q5 {
     public static void main(String[] args) {
         System.out.println("Enter Your Income to calculate you income tax");
         Scanner sc =new Scanner(System.in);
+        double tax;
 
         double income =sc.nextDouble();
         if(income <250000)
-            System.out.println("No Tax");
+            tax=0.0;
         else if(income <500000){
             System.out.println("You will pay 5%  Tax on "+income+" Income");
-            double tax=income/20;
-            System.out.println("Your Tax Amount is "+tax);
+             tax = (income - 250000) * 0.05;
         }
         else if(income <1000000){
-            System.out.println("You will pay 5%  Tax on "+income+" Income");
-            double tax=income*3/10;
-            System.out.println("Your Tax Amount is "+tax);
+            System.out.println("You will pay 20%  Tax on "+income+" Income");
+             tax = 12500 + (income - 500000) * 0.20;
 
         }
         else {
-            System.out.println("You will pay 5%  Tax on "+income+" Income");
-            double tax=income *3/10;
-            System.out.println("Your Tax Amount is "+tax);
+            System.out.println("You will pay 30%  Tax on "+income+" Income");
+            tax = 12500 + 100000 + (income - 1000000) * 0.30;
 
         }
+        System.out.println(tax);
 
 
     }
